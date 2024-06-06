a4c6e2996061f8a35f8648be7ef063e9a5b1d414
anushasureshDeveloper <anushasuresh2323@gmail.com>
2024-06-06 13:24:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [catfact,setCatFact]=useState("")
+
+  const fetchData=async()=>{
+    const {data}=await axios.get("https://catfact.ninja/fact")
+    setCatFact(data.fact)
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center text-blue-300  bg-black font-bold text-xl min-h-screen py-24">
+    <button onClick={fetchData} className="flex bg-yellow-500 text-black px-9 py-3 rounded-md mb-3" >Genarate Cat fact</button>
+{catfact}
     </div>
   );
 }
